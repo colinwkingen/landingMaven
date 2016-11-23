671d08db0d9e3bc337b35936258b432f704ef908
Colin Kingen <colinwkingen@gmail.com>
2016-11-22 19:18:19 -0800

    copy basic project files from previous gradle project

--- src/main/java/com/landing/App.java
+++ src/main/java/com/landing/App.java
@@ -1,13 +1,53 @@
-package com.landing;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+    if (process.environment().get("PORT") != null) {
+      port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+      port = 4567;
     }
+    setPort(port);
+
+    staticFileLocation("/public");
+
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/about", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/about.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/form", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/add_comment", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String comment = request.queryParams("comment");
+      String sender = request.queryParams("sender");
+      model.put("comment", comment);
+      model.put("sender", sender);
+      model.put("template", "templates/add_comment.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 }
